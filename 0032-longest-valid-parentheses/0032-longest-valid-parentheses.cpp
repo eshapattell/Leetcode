9c19e48daa09798b95c2788b512834ebba5065e5
eshapattell <eshapattell@gmail.com>
2026-07-17 08:59:39 +0530

    Time: 0 ms (100%), Space: 11.6 MB (73.43%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -3,29 +3,24 @@ public:
     int longestValidParentheses(string s) {
         stack<int> st;
         int n= s.size();
-        vector<int> visited(n);
+        st.push(-1);
+        int maxCount= 0;
 
         for(int i=0;i<n;i++){
             if(s[i]== '('){
                 st.push(i);
             }else{
-                if(!st.empty()){
-                    visited[i]=1;
-                    visited[st.top()]=1;
-                    st.pop();
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }else{
+                    maxCount= max(maxCount,i - st.top());
                 }
-            }
-        }
-        int maxCount=0;
-        int curr=0;
-        for(int i=0;i<n;i++){
-            if(visited[i]==1){
-                curr++;
-                maxCount= max(maxCount, curr);
-            }else{
-                curr=0;
             }
         }
         return maxCount;
     }
 };
+
+//time-complexity: O(n);
+//space-complexity: O(1);
