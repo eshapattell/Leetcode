00f14eef9e50f93658347c63dd7ffee7fb2e27a8
eshapattell <eshapattell@gmail.com>
2026-07-20 10:32:57 +0530

    Time: 91 ms (12.86%), Space: 193.1 MB (65.51%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n= arr.size();
+        vector<int> left(n), right(n);
+        stack <int> st;
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[st.top()] > arr[i]) {
+                st.pop();
+            }
+            if (st.empty())
+                left[i] = -1;
+            else
+                left[i] = st.top();
+
+            st.push(i);
+        }
+
+        while (!st.empty())
+            st.pop();
+
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+            if (st.empty())
+                right[i] = n;
+            else
+                right[i] = st.top();
+
+            st.push(i);
+        }
+
+        // for(auto &e: left) cout<<e<<" ";cout<<endl;
+        // for(auto &e: right) cout<<e<<" ";cout<<endl;
+        long long ans = 0;
+        int mod = 1e9 + 7;
+
+        for(int i=0;i<n;i++){
+            long long leftCount = i - left[i];
+            long long rightCount = right[i] - i;
+
+            ans= (ans + (leftCount * rightCount % mod)* arr[i] %mod) %mod;
+        }
+        return ans;
+
+    }
+};
