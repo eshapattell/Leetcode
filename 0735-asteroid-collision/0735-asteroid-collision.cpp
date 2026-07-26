71866bceece7fb33634ff0d075637c12212427ef
eshapattell <eshapattell@gmail.com>
2026-07-26 19:27:24 +0530

    Time: 3 ms (28.4%), Space: 22.1 MB (60.86%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -4,33 +4,42 @@ public:
         stack<int> st;
         vector<int> ans;
 
-        for(auto ch : asteroids){
-            if(ch >0){
+        for(int ch : asteroids){
+            //asteroid is positive and moving in right dir
+            if(ch > 0){
                 st.push(ch);
             }else{
-                bool destroyed =false;
-                while(!st.empty() && st.top() > 0){
+                //for asteroid moving towards left
+                //to check if curr asteroid is destroyed or not
+                bool destroyed= false;
+                while(!st.empty() && st.top() >0){
+                    //curr asteroid is larger
                     if(st.top() < abs(ch)){
-                            st.pop();
-                    }else if(st.top() == abs(ch)){
                         st.pop();
-                        destroyed =true;
+                    }else if(st.top() == abs(ch)){
+                        //both are of same size so both get destroyed
+                        st.pop();
+                        destroyed= true;
                         break;
                     }else{
+                        //the top element of the stack is bigger
                         break;
-                        destroyed= true;
+                        destroyed =true;
                     }
                 }
-                if(!destroyed && (st.empty() || st.top() < 0)){
+                //curr asteroid not destroyed and the stack is empty or top ele is also in left dir meaning it will never collide
+                if(!destroyed && (st.empty() || st.top() <0)){
                     st.push(ch);
                 }
             }
         }
+        //emptying the stack
         while(!st.empty()){
-                ans.push_back(st.top());
-                st.pop();
+            ans.push_back(st.top());
+            st.pop();
         }
-        reverse(ans.begin(), ans.end());
+        //reversing the stack output
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
