247cd4f546c40df4ebd2f0a13f764afa57fe0b0c
eshapattell <eshapattell@gmail.com>
2026-08-17 10:19:14 +0530

    Time: 0 ms (100%), Space: 10.5 MB (73.7%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,45 +1,50 @@
 class Solution {
 public:
-    bool checkValid(int minSum, int &k, vector<int> &nums) {
-        int sum = 0;
-        int totSub = 1;
-
-        for(int num: nums) {
-            sum += num;
-            if(sum > minSum) {
-                totSub++;
-                sum = num;
+    bool getAns(int mid, int k , vector<int>&nums){
+        //to add sum of subarray
+        int sum=0;
+        //to count how many ksubarray mid takes
+        int totalSub=1;
+        
+        for(auto num: nums){
+            //adding value to sum of subarray
+            sum+= num;
+            //if sum exceeds mid value then count of subarray inc
+            if(sum > mid){
+                totalSub++;
+                //to make the sum start again from the current nums[i]
+                sum=num;
             }
-            if(totSub > k) return false;
+            //if for current mid it takes more than k splits then its false
+            if(totalSub > k)return false;
         }
-
-        return totSub <= k;
+        //true
+        return totalSub <=k;
     }
-
     int splitArray(vector<int>& nums, int k) {
-        
-        int n = nums.size();
-        
-        int start = 0;
-        int end = 0;
-        int ans = 0;
+        int lo= 0;
+        int high=0;
+        int ans=0;
 
-        for(int i=0; i<n; i++) {
-            start = max(start, nums[i]);
-            end += nums[i];
+        for(int i=0;i< nums.size();i++){
+            //maximum element of the array
+            //k's min value can be 1 so in that case max element will be the lo
+            lo = max(lo, nums[i]);
+            //total sum of the array
+            high += nums[i];
         }
-
-        while(start <= end) {
-            int mid = start + (end - start)/2;
-
-            if(checkValid(mid, k, nums)) {
-                ans = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+        while(lo <= high){
+            int mid = lo + (high-lo)/2;
+            //if true then we will move left side as value greater than that will also be true
+            if(getAns(mid, k, nums)){
+                ans= mid;
+                high= mid-1;
+            }
+            //if false we move to right bcz value less than that will also false
+            else{
+                lo= mid+1;
             }
         }
-
         return ans;
     }
 };
