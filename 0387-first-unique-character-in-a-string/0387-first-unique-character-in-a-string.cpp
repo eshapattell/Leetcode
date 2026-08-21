b91e55d9a68a418ac423f4a0c89fda994bca6895
eshapattell <eshapattell@gmail.com>
2026-08-21 13:27:59 +0530

    Time: 11 ms (49.38%), Space: 14.8 MB (38.73%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -3,14 +3,16 @@ public:
     int firstUniqChar(string s) {
         unordered_map<char,int> mp;
 
-        for(auto ch: s){
-            mp[ch]++;
+        for(char c : s){
+            mp[c]++;
         }
-        for(int i=0;i<s.size();i++){
+
+        for(int i = 0; i < s.size(); i++){
             if(mp[s[i]] == 1){
                 return i;
             }
         }
+
         return -1;
     }
 };
