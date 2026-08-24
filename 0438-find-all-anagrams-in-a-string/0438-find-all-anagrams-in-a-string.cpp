f42f8757a9903b4e974464a0922d8692b5f01098
eshapattell <eshapattell@gmail.com>
2026-08-24 08:19:20 +0530

    Time: 29 ms (14.58%), Space: 14.8 MB (21.59%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,29 +1,53 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        int n1 = s.size(), n2 = p.size();
         vector<int> ans;
 
-        if (n1 < n2) return ans;
+        if(p.size() > s.size()) {
+            return ans;
+        }
+        unordered_map<char, int> target;
+        unordered_map<char, int> window;
 
-        vector<int> a(26, 0), b(26, 0);
-
-        for (int i = 0; i < n2; i++) {
-            a[p[i] - 'a']++;
-            b[s[i] - 'a']++;
+        // count frequency of every character in p
+        for(char ch : p) {
+            target[ch]++;
         }
 
-        if (a == b) ans.push_back(0);
+        // left pointer of sliding window
+        int left = 0;
 
-        for (int i = n2; i < n1; i++) {
-            b[s[i] - 'a']++;
-            b[s[i - n2] - 'a']--;
+        // right pointer moves through the string
+        for(int right = 0; right < s.size(); right++) {
 
-            if (a == b) {
-                ans.push_back(i - n2 + 1);
+            // add current character to the window
+            window[s[right]]++;
+
+            // if window becomes bigger than p,
+            // remove character from the left side
+            if(right - left + 1 > p.size()) {
+
+                // decrease frequency of left character
+                window[s[left]]--;
+
+                // if its frequency becomes 0,
+                // completely remove it from the map
+                if(window[s[left]] == 0) {
+                    window.erase(s[left]);
+                }
+
+                // move left pointer forward
+                left++;
+            }
+
+            // now window size is equal to p.size()
+            // if both frequency maps are same,
+            // current substring is an anagram of p
+            if(right - left + 1 == p.size() &&
+               window == target) {
+                ans.push_back(left);
             }
         }
-
         return ans;
     }
 };
