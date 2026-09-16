7c684af75ea855edd13bae7243d4d4a9d346f4ab
eshapattell <eshapattell@gmail.com>
2026-09-17 01:19:41 +0530

    Time: 59 ms (37.92%), Space: 89.4 MB (52.66%) - LeetHub

--- 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
+++ 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int waysToSplit(vector<int>& nums) {
-        constexpr int MOD = 1e9 + 7;
-        int res = 0, n = nums.size();
-        partial_sum(nums.begin() , nums.end() , nums.begin());
-        int total = nums[n - 1];
-        for(int i = 0, j = 0, k = 0; i < n - 2; ++i){
-            int left = nums[i];
-            j = max(i + 1 , j);
-            while(j < n - 1 && nums[j] < 2 * left) ++j;
-            k = max(j , k);
-            while(k < n - 1 && nums[k] - left <= total - nums[k]) ++k;
-            res = (res + k - j) % MOD;
+        int n=nums.size();
+        int ans=0;
+        vector<int> pre(n,0);
+           pre[0]=nums[0];
+        for(int i=1;i<n;i++)
+        pre[i]=pre[i-1]+nums[i];
+        int mod=1e9+7;
+        // 1 3 5 7 12 12 
+        for(int i=0;i<n;i++)
+        {
+           int low=lower_bound(pre.begin()+i+1,pre.end(),2*pre[i])-pre.begin();//it will return min boundary if we have middle subarray sum equal to first sub array sum(we are checking 2*pre[i] because it is prefix sum and we do not have to conside first subarray sum i.e pre[i])
+           int up=upper_bound(pre.begin()+i+1,pre.begin()+n-1,pre[i]+((pre[n-1]-pre[i])/2))-pre.begin();//max for second subarray will (remaing part)/2 
+           ans+=(max(0,(up-low)))%mod;
+           ans=ans%mod;
         }
-        return res;
+      return ans;
     }
 };
